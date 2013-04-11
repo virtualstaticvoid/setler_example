eb1baea61352a1f6779dd881f2d89ade2f9f60ac
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2013-04-11 22:44:35 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SetlerExample::Application.config.secret_token = '582e7d2ddaafca82f28baec5cde0ea043f7df8e6d87e38dedd1ec9079a27ad92ad979562d90565797edffea22055ceb2079908f811bea8968b5fdd5bedc1297e'
